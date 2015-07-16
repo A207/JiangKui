d64f3ce9ace70292453ce2c11a93667337513392
JIANGKUIX <240654807@qq.com>
2015-07-16 15:48:37 +0800

    go

---
+++ TulingDemo/TulingApp/app/src/main/java/com/cd/tulingapp/HttpGetDataListener.java
@@ -0,0 +1,8 @@
+package com.cd.tulingapp;
+
+/**
+ * Created by Administrator on 2015/7/14.
+ */
+public interface HttpGetDataListener {
+    void getDataUrl(String data);
+}
---
+++ TulingDemo/TulingApp/app/src/main/java/com/cd/tulingapp/ListData.java
@@ -0,0 +1,36 @@
+package com.cd.tulingapp;
+
+/**
+ * Created by Administrator on 2015/7/14.
+ */
+public  class ListData {
+    private String content;
+    public static final int SEND=1;
+    public static final int RECEIVER=2;
+    private int flag;
+    private String time;
+    public ListData(String content,int flag,String time){
+        setContent(content);
+        setFlag(flag);
+        setTime(time);
+    }
+    public String getTime() {
+        return time;
+    }
+    private void setTime(String time) {
+    this.time=time;
+    }
+    public int getFlag() {
+        return flag;
+    }
+    public void setFlag(int flag) {
+
+        this.flag = flag;
+    }
+    public String getContent(){
+        return content;
+    }
+    public void setContent(String content){
+        this.content=content;
+    }
+}
